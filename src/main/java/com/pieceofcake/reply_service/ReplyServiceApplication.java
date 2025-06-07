d52edff2c2ab7ac2821eeeffef2930d7fee52b77
Eunseo <kendyjenny@naver.com>
2025-06-07 14:47:43 +0900

    [feat] reply-service 기본 설정

---
+++ src/main/java/com/pieceofcake/reply_service/ReplyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pieceofcake.reply_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyServiceApplication.class, args);
+	}
+
+}
