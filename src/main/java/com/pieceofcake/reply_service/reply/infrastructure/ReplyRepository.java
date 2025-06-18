a8e911b51f8e9c554ef4170e368cc3cc621e125b
Eunseo <kendyjenny@naver.com>
2025-06-18 14:52:41 +0900

    [fix] : ReplyRepository Query 추가

--- src/main/java/com/pieceofcake/reply_service/reply/infrastructure/ReplyRepository.java
+++ src/main/java/com/pieceofcake/reply_service/reply/infrastructure/ReplyRepository.java
@@ -2,6 +2,7 @@ package com.pieceofcake.reply_service.reply.infrastructure;
 
 import com.pieceofcake.reply_service.reply.domain.Reply;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +13,6 @@ public interface ReplyRepository extends MongoRepository<Reply, String> {
 
     List<Reply> findByParentReplyUuidAndDeletedFalse(String parentReplyUuid);
 
+    @Query("{ 'replyUuid': ?0, 'deleted': false }")
     Optional<Reply> findByReplyUuidAndDeletedFalse(String replyUuid);
 }
