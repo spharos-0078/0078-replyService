173af012e17ee2d8d632852f2ecf0e30a96fc122
Eunseo <kendyjenny@naver.com>
2025-07-13 14:22:32 +0900

    [fix]: 댓글 전체조회 시간 반환하도록 수정

--- src/main/java/com/pieceofcake/reply_service/reply/dto/out/GetCommunityReplyUuidResponseDto.java
+++ src/main/java/com/pieceofcake/reply_service/reply/dto/out/GetCommunityReplyUuidResponseDto.java
@@ -6,23 +6,34 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class GetCommunityReplyUuidResponseDto {
 
     private String replyUuid;
     private boolean deleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     @Builder
-    public GetCommunityReplyUuidResponseDto(String replyUuid, boolean deleted) {
+    public GetCommunityReplyUuidResponseDto(
+            String replyUuid, boolean deleted,
+            LocalDateTime createdAt, LocalDateTime updatedAt
+    ) {
         this.replyUuid = replyUuid;
         this.deleted = deleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public static GetCommunityReplyUuidResponseDto from(Reply reply) {
         return GetCommunityReplyUuidResponseDto.builder()
                 .replyUuid(reply.getReplyUuid())
                 .deleted(reply.isDeleted())
+                .createdAt(reply.getCreatedAt())
+                .updatedAt(reply.getUpdatedAt())
                 .build();
     }
 
@@ -30,6 +41,8 @@ public class GetCommunityReplyUuidResponseDto {
         return GetCommunityReplyUuidResponseVo.builder()
                 .replyUuid(this.replyUuid)
                 .deleted(this.deleted)
+                .createdAt(this.createdAt)
+                .updatedAt(this.updatedAt)
                 .build();
     }
 }
--- src/main/java/com/pieceofcake/reply_service/reply/vo/out/GetCommunityReplyUuidResponseVo.java
+++ src/main/java/com/pieceofcake/reply_service/reply/vo/out/GetCommunityReplyUuidResponseVo.java
@@ -4,19 +4,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class GetCommunityReplyUuidResponseVo {
 
     private String replyUuid;
     private boolean deleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     @Builder
     public GetCommunityReplyUuidResponseVo(
             String replyUuid,
-            boolean deleted
+            boolean deleted,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
     ) {
         this.replyUuid = replyUuid;
         this.deleted = deleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
